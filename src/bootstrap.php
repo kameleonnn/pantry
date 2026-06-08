b14ee1cee0863a4f1688929a0503b53831d4bc10
kameleonnn <kameleonnn@codeberg.org>
2026-06-08 23:24:46 +0200

    something with sessions

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 define('ASSET_PATH', getenv('DB_HOST') ? '': '/public');
-session_start();
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/commons/view.php';
 require_once __DIR__ . '/inc/flash.php';
