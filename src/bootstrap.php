8e52fa475369a0e19cc656913b66bfd5437d99cb
kameleonnn <eryk.kozub@op.pl>
2026-06-09 00:06:37 +0000

    Update bootstrap.php

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,10 @@
 <?php
-define('ASSET_PATH', getenv('DB_HOST') ? '': '/public');
-session_start();
+if (!defined('ASSET_PATH')) {
+    define('ASSET_PATH', getenv('DB_HOST') ? '' : '/public');
+}
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/commons/view.php';
 require_once __DIR__ . '/inc/flash.php';
